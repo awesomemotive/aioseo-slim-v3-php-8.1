bnf/Slim

Refactor middleware routing into a stack based dispatch cycle
Improve certain things:
 * Do not hide container dependencies in the CallableResolverInterface
   getContainer method (this method is dropped now)
 * Use a middleware stack instead of a queue, as slim used to use before #2555
 * Use designated request handlers as kernel (as we know them from slim v3)
   instead of middlewares that ignore the second (RequestHandler) parameter
 * Do not recreate middlewares in route/route-group middleware combination,
   rather chain middleware dispatchers, thanks to the (now) proper use of
   RequestHandlerInterface of the MiddlewareDispatcher which allows itself
   to be chained into another middleware stack
 * Drop some helper middlewares, which is good as they were applicable only
   as internal middleware anyway. This logic is now handled in the middleware
   dispatcher for optimization reasons (because that handler
   create a RequestHandler wrapper for every middleware, it's most performant
   to include lazy/closure dispatch logic in the RequestHandler wrappers).
 * DispatchMiddleware (and it's corresponding test) is moved into the
   Router which now acts as a PSR-15 RequestHandler
 * Replace middleware related tests that test internal state (e.g. whether
   something is at the bottom of a list.) That is an implementation detail and
   should rather be verified by checking for proper operation
 * Rename MiddlewareRunner to MiddlewareDispatcher, as it actually dispatches
   middlewares now

Fixes: #2590

Slim/RouteGroup.php
@@ -24,6 +24,13 @@ use Slim\Interfaces\RouteGroupInterface;
 class RouteGroup extends Routable implements RouteGroupInterface
 {
     /**
+     * middlewares
+     *
+     * @var array
+     */
+    protected $middlewares = [];
+
+    /**
      * Create a new RouteGroup
      *
      * @param string                    $pattern  The pattern prefix for the group
@@ -41,7 +48,6 @@ class RouteGroup extends Routable implements RouteGroupInterface
         $this->callable = $callable;
         $this->responseFactory = $responseFactory;
         $this->callableResolver = $callableResolver;
-        $this->middlewareRunner = new MiddlewareRunner();
     }
 
     /**
@@ -50,7 +56,7 @@ class RouteGroup extends Routable implements RouteGroupInterface
      */
     public function add($middleware): RouteGroupInterface
     {
-        $this->addRouteMiddleware($middleware);
+        $this->middlewares[] = $middleware;
         return $this;
     }
 
@@ -60,8 +66,18 @@ class RouteGroup extends Routable implements RouteGroupInterface
      */
     public function addMiddleware(MiddlewareInterface $middleware): RouteGroupInterface
     {
-        $this->addRouteMiddleware($middleware);
+        $this->middlewares[] = $middleware;
         return $this;
+    }
+
+    /**
+     * Get the middleware registered for the group
+     *
+     * @return mixed[]
+     */
+    public function getMiddleware(): array
+    {
+        return $this->middlewares;
     }
 
     /**

tests/MiddlewareDispatcherTest.php
@@ -1,0 +1,125 @@
+<?php
+/**
+ * Slim Framework (https://slimframework.com)
+ *
+ * @link      https://github.com/slimphp/Slim
+ * @copyright Copyright (c) 2011-2018 Josh Lockhart
+ * @license   https://github.com/slimphp/Slim/blob/4.x/LICENSE.md (MIT License)
+ */
+namespace Slim\Tests;
+
+use Pimple\Container as Pimple;
+use Pimple\Psr11\Container as Psr11Container;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Slim\MiddlewareDispatcher;
+use Slim\Tests\Mocks\MockMiddlewareWithoutConstructor;
+use Slim\Tests\Mocks\MockRequestHandler;
+use stdClass;
+
+/**
+ * Class MiddlewareDispatcherTest
+ * @package Slim\Tests
+ */
+class MiddlewareDispatcherTest extends TestCase
+{
+    public function testAddMiddleware()
+    {
+        $responseFactory = $this->getResponseFactory();
+        $callable = function ($request, $handler) use ($responseFactory) {
+            return $responseFactory->createResponse();
+        };
+
+        $middlewareDispatcher = new MiddlewareDispatcher($this->createMock(RequestHandlerInterface::class));
+        $middlewareDispatcher->add($callable);
+
+        $response = $middlewareDispatcher->handle($this->createMock(ServerRequestInterface::class));
+        $this->assertInstanceOf(ResponseInterface::class, $response);
+    }
+
+    public function testNamedFunctionIsResolved()
+    {
+        function testProcessRequest(ServerRequestInterface $request, RequestHandlerInterface $handler)
+        {
+            return $handler->handle($request);
+        }
+
+        $handler = new MockRequestHandler();
+        $middlewareDispatcher = new MiddlewareDispatcher($handler);
+        $middlewareDispatcher->addLazy(__NAMESPACE__ . '\testProcessRequest');
+
+        $request = $this->createServerRequest('/');
+        $middlewareDispatcher->handle($request);
+
+        $this->assertEquals(1, $handler->getCalledCount());
+    }
+
+    public function testDeferredResolvedCallable()
+    {
+        $pimple = new Pimple();
+        $pimple['callable'] = function () {
+            return function (ServerRequestInterface $request, RequestHandlerInterface $handler) {
+                return $handler->handle($request);
+            };
+        };
+        $container = new Psr11Container($pimple);
+
+        $handler = new MockRequestHandler();
+        $middlewareDispatcher = new MiddlewareDispatcher($handler, $container);
+        $middlewareDispatcher->addLazy('callable');
+
+        $request = $this->createServerRequest('/');
+        $middlewareDispatcher->handle($request);
+
+        $this->assertEquals(1, $handler->getCalledCount());
+    }
+
+    public function testResolvableReturnsInstantiatedObject()
+    {
+        MockMiddlewareWithoutConstructor::$CalledCount = 0;
+
+        $handler = new MockRequestHandler();
+        $middlewareDispatcher = new MiddlewareDispatcher($handler);
+        $middlewareDispatcher->addLazy(MockMiddlewareWithoutConstructor::class);
+
+        $request = $this->createServerRequest('/');
+        $middlewareDispatcher->handle($request);
+
+        $this->assertEquals(1, MockMiddlewareWithoutConstructor::$CalledCount);
+        $this->assertEquals(1, $handler->getCalledCount());
+    }
+
+    /**
+     * @expectedException \RuntimeException
+     * @expectedExceptionMessage MiddlewareInterfaceNotImplemented is not resolvable
+     */
+    public function testResolveThrowsExceptionWhenResolvableDoesNotImplementMiddlewareInterface()
+    {
+        $pimple = new Pimple();
+        $pimple['MiddlewareInterfaceNotImplemented'] = new stdClass();
+        $container = new Psr11Container($pimple);
+
+        $handler = new MockRequestHandler();
+        $middlewareDispatcher = new MiddlewareDispatcher($handler, $container);
+        $middlewareDispatcher->addLazy('MiddlewareInterfaceNotImplemented');
+
+        $request = $this->createServerRequest('/');
+        $middlewareDispatcher->handle($request);
+    }
+
+    /**
+     * @expectedException \RuntimeException
+     * @expectedExceptionMessage Unresolvable::class is not resolvable
+     */
+    public function testResolveThrowsExceptionWithoutContainerAndUnresolvableClass()
+    {
+        $handler = new MockRequestHandler();
+        $middlewareDispatcher = new MiddlewareDispatcher($handler);
+        $middlewareDispatcher->addLazy('Unresolvable::class');
+
+        $request = $this->createServerRequest('/');
+        $middlewareDispatcher->handle($request);
+    }
+}

awesomemotive/aioseo-slim-v3-php-8.1

   instead of middleware that ignore the second (RequestHandler) parameter
 * Do not recreate middleware in route/route-group middleware combination,
 * Drop some helper middleware, which is good as they were applicable only
   middleware now

Slim/RouteGroup.php
@@ -24,6 +24,13 @@ use Slim\Interfaces\RouteGroupInterface;
 class RouteGroup extends Routable implements RouteGroupInterface
 {
     /**
+     * middleware
+     *
+     * @var array
+     */
+    protected $middleware = [];
+
+    /**
      * Create a new RouteGroup
      *
      * @param string                    $pattern  The pattern prefix for the group
@@ -41,7 +48,6 @@ class RouteGroup extends Routable implements RouteGroupInterface
         $this->callable = $callable;
         $this->responseFactory = $responseFactory;
         $this->callableResolver = $callableResolver;
-        $this->middlewareRunner = new MiddlewareRunner();
     }
 
     /**
@@ -50,7 +56,7 @@ class RouteGroup extends Routable implements RouteGroupInterface
      */
     public function add($middleware): RouteGroupInterface
     {
-        $this->addRouteMiddleware($middleware);
+        $this->middleware[] = $middleware;
         return $this;
     }
 
@@ -60,8 +66,18 @@ class RouteGroup extends Routable implements RouteGroupInterface
      */
     public function addMiddleware(MiddlewareInterface $middleware): RouteGroupInterface
     {
-        $this->addRouteMiddleware($middleware);
+        $this->middleware[] = $middleware;
         return $this;
+    }
+
+    /**
+     * Get the middleware registered for the group
+     *
+     * @return mixed[]
+     */
+    public function getMiddleware(): array
+    {
+        return $this->middleware;
     }
 
     /**

tests/MiddlewareDispatcherTest.php
@@ -1,0 +1,125 @@
+<?php
+/**
+ * Slim Framework (https://slimframework.com)
+ *
+ * @link      https://github.com/slimphp/Slim
+ * @copyright Copyright (c) 2011-2018 Josh Lockhart
+ * @license   https://github.com/slimphp/Slim/blob/4.x/LICENSE.md (MIT License)
+ */
+namespace Slim\Tests;
+
+use Pimple\Container as Pimple;
+use Pimple\Psr11\Container as Psr11Container;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Slim\MiddlewareDispatcher;
+use Slim\Tests\Mocks\MockMiddlewareWithoutConstructor;
+use Slim\Tests\Mocks\MockRequestHandler;
+use stdClass;
+
+/**
+ * Class MiddlewareDispatcherTest
+ * @package Slim\Tests
+ */
+class MiddlewareDispatcherTest extends TestCase
+{
+    public function testAddMiddleware()
+    {
+        $responseFactory = $this->getResponseFactory();
+        $callable = function ($request, $handler) use ($responseFactory) {
+            return $responseFactory->createResponse();
+        };
+
+        $middlewareDispatcher = new MiddlewareDispatcher($this->createMock(RequestHandlerInterface::class));
+        $middlewareDispatcher->add($callable);
+
+        $response = $middlewareDispatcher->handle($this->createMock(ServerRequestInterface::class));
+        $this->assertInstanceOf(ResponseInterface::class, $response);
+    }
+
+    public function testNamedFunctionIsResolved()
+    {
+        function testProcessRequest(ServerRequestInterface $request, RequestHandlerInterface $handler)
+        {
+            return $handler->handle($request);
+        }
+
+        $handler = new MockRequestHandler();
+        $middlewareDispatcher = new MiddlewareDispatcher($handler);
+        $middlewareDispatcher->addDeferred(__NAMESPACE__ . '\testProcessRequest');
+
+        $request = $this->createServerRequest('/');
+        $middlewareDispatcher->handle($request);
+
+        $this->assertEquals(1, $handler->getCalledCount());
+    }
+
+    public function testDeferredResolvedCallable()
+    {
+        $pimple = new Pimple();
+        $pimple['callable'] = function () {
+            return function (ServerRequestInterface $request, RequestHandlerInterface $handler) {
+                return $handler->handle($request);
+            };
+        };
+        $container = new Psr11Container($pimple);
+
+        $handler = new MockRequestHandler();
+        $middlewareDispatcher = new MiddlewareDispatcher($handler, $container);
+        $middlewareDispatcher->addDeferred('callable');
+
+        $request = $this->createServerRequest('/');
+        $middlewareDispatcher->handle($request);
+
+        $this->assertEquals(1, $handler->getCalledCount());
+    }
+
+    public function testResolvableReturnsInstantiatedObject()
+    {
+        MockMiddlewareWithoutConstructor::$CalledCount = 0;
+
+        $handler = new MockRequestHandler();
+        $middlewareDispatcher = new MiddlewareDispatcher($handler);
+        $middlewareDispatcher->addDeferred(MockMiddlewareWithoutConstructor::class);
+
+        $request = $this->createServerRequest('/');
+        $middlewareDispatcher->handle($request);
+
+        $this->assertEquals(1, MockMiddlewareWithoutConstructor::$CalledCount);
+        $this->assertEquals(1, $handler->getCalledCount());
+    }
+
+    /**
+     * @expectedException \RuntimeException
+     * @expectedExceptionMessage MiddlewareInterfaceNotImplemented is not resolvable
+     */
+    public function testResolveThrowsExceptionWhenResolvableDoesNotImplementMiddlewareInterface()
+    {
+        $pimple = new Pimple();
+        $pimple['MiddlewareInterfaceNotImplemented'] = new stdClass();
+        $container = new Psr11Container($pimple);
+
+        $handler = new MockRequestHandler();
+        $middlewareDispatcher = new MiddlewareDispatcher($handler, $container);
+        $middlewareDispatcher->addDeferred('MiddlewareInterfaceNotImplemented');
+
+        $request = $this->createServerRequest('/');
+        $middlewareDispatcher->handle($request);
+    }
+
+    /**
+     * @expectedException \RuntimeException
+     * @expectedExceptionMessage Unresolvable::class is not resolvable
+     */
+    public function testResolveThrowsExceptionWithoutContainerAndUnresolvableClass()
+    {
+        $handler = new MockRequestHandler();
+        $middlewareDispatcher = new MiddlewareDispatcher($handler);
+        $middlewareDispatcher->addDeferred('Unresolvable::class');
+
+        $request = $this->createServerRequest('/');
+        $middlewareDispatcher->handle($request);
+    }
+}